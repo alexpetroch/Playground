4139af165d42ea15ab6cb1c1cc25ea810eccd905
alexpetroch <petroch@mail.ru>
2018-02-26 22:05:42 +0300

    Tree

--- Playground/Interview/TreeQ.cs
+++ Playground/Interview/TreeQ.cs
@@ -237,5 +237,31 @@ namespace Playground.Interview
 
             return false;
         }
+
+        /// <summary>
+        /// Given a root of a Binary Search Tree (BST) and a number num, 
+        /// implement an efficient function findLargestSmallerKey that finds the largest key in the tree that is smaller than num. If such a number doesn’t exist, return -1. Assume that all keys in the tree are nonnegative.
+        /// </summary>
+        private int FindLargestSmallerKey(Tree<int>.Node root, int num)
+        {
+            // Time complexity -> O(h) means that O(n) - worst case
+            Tree<int>.Node node = root;
+            Tree<int>.Node targetNode = null;
+
+            while (node != null)
+            {
+                if (node.Value < num)
+                {
+                    targetNode = node;
+                    node = node.Right;
+                }
+                else
+                {
+                    node = node.Left;
+                }
+            }
+
+            return targetNode != null ? targetNode.Value : -1;
+        }
     }
 }
